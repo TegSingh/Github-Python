108db4a9f92ae5bf3cba5208bfafb86fe3341fe4
TegSingh <tegveer2211@gmail.com>
2021-08-12 01:24:18 -0400

    virtualenv + gitpy initialize

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Github-Python
-Repository to read commit details and file strucutre of Open source code
+This is a Python utility to list commits for any open source project
+This program helps in reading commit details and file strucutre of Open source code
--- script.py
+++ script.py
@@ -1,22 +1,38 @@
 import csv
+from git import Repo
 
-try: 
-    f = open('commit_info.csv', 'w')
-    print("File opened successfully")
-except: 
-    print("Error opening file")
+def main(): 
+    try: 
+        repo = Repo() 
+        print("Repo instance loaded successfully")
+    except: 
+        print("Couldn't load repo instance")
 
-try: 
-    writer = csv.writer(f)
-except: 
-    print("Error creating writer")
+    try: 
+        f = open('commit_info.csv', 'w')
+        print("File opened successfully")
+    except: 
+        print("Error opening file")
 
-tableheader = ['name', 'age']
-data = ['Tegveer', 20]
+    try: 
+        writer = csv.writer(f)
+        print("Writer created successfully")
+    except: 
+        print("Error creating writer"),
 
-try:
-    writer.writerow(tableheader)
-    writer.writerow(data)
-except:
-    print("Error writing to csv file")
-f.close()
+    tableheader = ['name', 'age']
+    data = [['Tegveer', 20], 
+            ['Hello world', 21],
+            ['lorem', 23], 
+            ['ipsum', 19]]
+
+    try:
+        writer.writerow(tableheader)
+        writer.writerows(data)
+        print("Rows written sucessfully")
+    except:
+        print("Error writing to csv file")
+    f.close()
+
+if __name__ == '__main__': 
+    main()
